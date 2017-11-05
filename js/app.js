1a248cb499d07c4960c6c8d1ceae7be8b7f19422
Sandra Israel-Ovirih <sandraisraelo@gmail.com>
2017-11-05 12:17:43 +0100

    hide full menu on close

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
 var menuIcon = $('.icon');
 var fullMenu = $('.full-menu')
-
+var closeIcon = $('#close-icon')
 
 menuIcon.on('click', function(){
     menuIcon.hide();
     fullMenu.show();
 })
 
-.
+
+
+closeIcon.on('click', function(){
+    fullMenu.hide();
+    menuIcon.show();
+})
